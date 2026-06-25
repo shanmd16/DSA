8f6c61e1ba63f3b61e19018c689903ac76c65ffa
shanmd16 <shanmoh1602@gmail.com>
2026-06-25 15:18:37 +0530

    add NumberOf1Bits

---
+++ Binary/NumberOf1Bits.java
@@ -0,0 +1,13 @@
+package Binary;
+
+public class NumberOf1Bits {
+    public static void main(String[] args) {
+        int n=11;
+        int count = 0;
+        while(n!=0){
+            count +=(n&1);
+            n>>>=1;
+        }
+        System.out.println(count);
+    }
+}
